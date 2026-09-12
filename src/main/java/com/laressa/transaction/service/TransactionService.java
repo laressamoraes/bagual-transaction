704418048d4da71e3e18063da23e1c766fc51f28
laressamoraes <laressa.luisy@hotmail.com>
2026-09-12 02:37:51 -0300

    feat: adiciona service

--- src/main/java/com/laressa/transaction/service/TransactionService.java
+++ src/main/java/com/laressa/transaction/service/TransactionService.java
@@ -1,7 +1,13 @@
 package com.laressa.transaction.service;
 
+import com.laressa.transaction.domain.Transaction;
+import com.laressa.transaction.dto.TransactionRequestDTO;
+import com.laressa.transaction.exception.TransactionNotFoundException;
 import com.laressa.transaction.repository.TransactionRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
 
 @Service
 public class TransactionService {
@@ -10,5 +16,24 @@ public class TransactionService {
 
     public TransactionService(TransactionRepository transactionRepository) {
         this.transactionRepository = transactionRepository;
+    }
+
+    public Transaction createTransaction(TransactionRequestDTO request) {
+        Transaction transaction = new Transaction(
+                request.transactionType(),
+                request.originAccountId(),
+                request.destinationAccountId(),
+                request.amount()
+        );
+        return transactionRepository.save(transaction);
+    }
+
+    public Transaction findById(UUID transactionId) {
+        return transactionRepository.findById(transactionId)
+                .orElseThrow(() -> new TransactionNotFoundException("Transação não encontrada: " + transactionId));
+    }
+
+    public List<Transaction> findAll() {
+        return transactionRepository.findAll();
     }
 }
