79752f7cb4ba69f422b3a974b094a7aa80279454
laressamoraes <laressa.luisy@hotmail.com>
2026-09-12 02:09:15 -0300

    feat: criação, busca por id e listagem de transações

--- src/main/java/com/laressa/transaction/controller/TransactionController.java
+++ src/main/java/com/laressa/transaction/controller/TransactionController.java
@@ -1,8 +1,16 @@
 package com.laressa.transaction.controller;
 
+import com.laressa.transaction.domain.Transaction;
+import com.laressa.transaction.dto.TransactionRequestDTO;
+import com.laressa.transaction.dto.TransactionResponseDTO;
 import com.laressa.transaction.service.TransactionService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/transactions")
@@ -12,5 +20,26 @@ public class TransactionController {
 
     public TransactionController(TransactionService transactionService) {
         this.transactionService = transactionService;
+    }
+
+    @PostMapping
+    public ResponseEntity<TransactionResponseDTO> createTransaction(@Valid @RequestBody TransactionRequestDTO transactionRequest) {
+        Transaction transaction = transactionService.createTransaction(transactionRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body(TransactionResponseDTO.fromEntity(transaction));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TransactionResponseDTO> findById(@PathVariable UUID id) {
+        Transaction transaction = transactionService.findById(id);
+        return ResponseEntity.ok(TransactionResponseDTO.fromEntity(transaction));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TransactionResponseDTO>> findAll() {
+        List<TransactionResponseDTO> transactions = transactionService.findAll()
+                .stream()
+                .map(TransactionResponseDTO::fromEntity)
+                .toList();
+        return ResponseEntity.ok(transactions);
     }
 }
