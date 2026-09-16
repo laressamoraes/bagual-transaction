9cd143f784bed5dd73d22b49c64fd55f79a732ab
laressamoraes <laressa.luisy@hotmail.com>
2026-09-16 20:37:23 -0300

    feat: adiciona containerização da aplicação Transaction

--- src/main/java/com/laressa/transaction/config/AccountClientConfig.java
+++ src/main/java/com/laressa/transaction/config/AccountClientConfig.java
@@ -1,5 +1,6 @@
 package com.laressa.transaction.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
@@ -7,10 +8,13 @@ import org.springframework.web.client.RestClient;
 @Configuration
 public class AccountClientConfig {
 
+    @Value("${account.service.url}")
+    private String accountServiceUrl;
+
     @Bean
     public RestClient accountRestClient() {
         return RestClient.builder()
-                .baseUrl("http://localhost:8081")
+                .baseUrl(accountServiceUrl)
                 .build();
     }
 }
